e1e0ae5c905725071748651e1f7d08b279ed11b3
nwillc <nwillc@gmail.com>
2021-12-27 20:09:47 -0500

    mostly fluent

---
+++ examples_test.go
@@ -0,0 +1,239 @@
+/*
+ *  Copyright (c) 2021,  nwillc@gmail.com
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package genfuncs_test
+
+import (
+	"fmt"
+	"github.com/nwillc/genfuncs"
+	"os"
+	"strings"
+	"testing"
+	"time"
+)
+
+var floatCmp = genfuncs.OrderedComparator[float32]()
+var greaterThanZero = genfuncs.IsGreaterThan(0)
+var intCmp = genfuncs.OrderedComparator[int]()
+var lexicalOrder = genfuncs.OrderedComparator[string]()
+var reverseLexical = genfuncs.ReverseComparator(lexicalOrder)
+var wordPositions = map[string]int{"hello": 1, "world": 2}
+var words genfuncs.Slice[string] = []string{"hello", "world"}
+
+func TestFunctionExamples(t *testing.T) {
+	if _, ok := os.LookupEnv("RUN_EXAMPLES"); !ok {
+		t.Skip("skipping: RUN_EXAMPLES not set")
+	}
+	// Functions
+	ExampleOrderedComparator()
+	ExampleReverseComparator()
+	ExampleStringerStringer()
+	ExampleFunctionComparator()
+	// Heap
+	ExampleNewHeap()
+	// Map
+	ExampleKeys()
+	ExampleValues()
+	// Slice fluent
+	ExampleSlice_All()
+	ExampleSlice_Any()
+	ExampleSlice_Contains()
+	ExampleSlice_Filter()
+	ExampleSlice_Find()
+	ExampleSlice_FindLast()
+	ExampleSlice_JoinToString()
+	ExampleSlice_SortBy()
+	ExampleSlice_Swap()
+	// slice functions
+	ExampleAssociate()
+	ExampleAssociateWith()
+	ExampleDistinct()
+	ExampleFold()
+	ExampleFlatMap()
+	ExampleGroupBy()
+	ExampleMap()
+	// Sort
+	ExampleSort()
+}
+
+func ExampleOrderedComparator() {
+	fmt.Println(lexicalOrder("a", "b")) // -1
+	fmt.Println(lexicalOrder("a", "a")) // 0
+	fmt.Println(lexicalOrder("b", "a")) // 1
+}
+
+func ExampleReverseComparator() {
+	fmt.Println(lexicalOrder("a", "b"))   // -1
+	fmt.Println(reverseLexical("a", "b")) // 1
+}
+
+func ExampleStringerStringer() {
+	var epoch time.Time
+	fmt.Println(epoch.String()) // 0001-01-01 00:00:00 +0000 UTC
+	stringer := genfuncs.StringerStringer[time.Time]()
+	fmt.Println(stringer(epoch)) // 0001-01-01 00:00:00 +0000 UTC
+}
+
+func ExampleFunctionComparator() {
+	var unixTime = func(t time.Time) int64 { return t.Unix() }
+	var timeComparator = genfuncs.FunctionComparator(unixTime, genfuncs.OrderedComparator[int64]())
+
+	now := time.Now()
+	fmt.Println(timeComparator(now, now.Add(time.Second))) // -1
+}
+
+func ExampleNewHeap() {
+	heap := genfuncs.NewHeap(intCmp)
+	heap.PushAll(3, 1, 4, 2)
+	for heap.Len() > 0 {
+		fmt.Print(heap.Pop()) // 1234
+	}
+	fmt.Println()
+}
+
+func ExampleKeys() {
+	keys := genfuncs.Keys(wordPositions)
+	fmt.Println(keys) // [hello, world]
+}
+
+func ExampleValues() {
+	values := genfuncs.Values(wordPositions)
+	fmt.Println(values) // [1, 2]
+}
+
+func ExampleSlice_All() {
+	var numbers genfuncs.Slice[int] = []int{1, 2, 3, 4}
+	fmt.Println(numbers.All(greaterThanZero)) // true
+}
+
+func ExampleSlice_Any() {
+	var fruits genfuncs.Slice[string] = []string{"apple", "banana", "grape"}
+	isApple := genfuncs.IsEqualTo("apple")
+	isPear := genfuncs.IsEqualTo("pear")
+	fmt.Println(fruits.Any(isApple)) // true
+	fmt.Println(fruits.Any(isPear))  // false
+}
+
+func ExampleSlice_Contains() {
+	var floats genfuncs.Slice[float32] = []float32{1.0, .5, 42}
+	fmt.Println(floats.Contains(.5, floatCmp))    // true
+	fmt.Println(floats.Contains(3.142, floatCmp)) // false
+}
+
+func ExampleSlice_Filter() {
+	var values genfuncs.Slice[int] = []int{1, -2, 2, -3}
+	fmt.Println(values.Filter(greaterThanZero)) // [1 2]
+}
+
+func ExampleSlice_Find() {
+	var values genfuncs.Slice[int] = []int{-1, -2, 2, -3}
+	fmt.Println(values.Find(greaterThanZero)) // 2 true
+}
+
+func ExampleSlice_FindLast() {
+	var values genfuncs.Slice[int] = []int{-1, -2, 2, 3}
+	fmt.Println(values.FindLast(greaterThanZero)) // 3 true
+}
+
+func ExampleSlice_JoinToString() {
+	var toString genfuncs.Stringer[string] = func(s string) string { return s }
+	fmt.Println(words.JoinToString(
+		toString,
+		", ",
+		"{",
+		"}",
+	)) // {hello, world}
+}
+
+func ExampleSlice_SortBy() {
+	var numbers genfuncs.Slice[int] = []int{1, 0, 9, 6, 0}
+	sorted := numbers.SortBy(intCmp)
+	fmt.Println(numbers) // [1 0 9 6 0]
+	fmt.Println(sorted)  // [0 0 1 6 9]
+}
+
+func ExampleSlice_Swap() {
+	words.Swap(0, 1)
+	fmt.Println(words) // [world hello]
+}
+
+// slice functions
+
+func ExampleAssociate() {
+	byLastName := func(n string) (string, string) {
+		parts := strings.Split(n, " ")
+		return parts[1], n
+	}
+	names := []string{"fred flintstone", "barney rubble"}
+	nameMap := genfuncs.Associate(names, byLastName)
+	fmt.Println(nameMap["rubble"]) // barney rubble
+}
+
+func ExampleAssociateWith() {
+	oddEven := func(i int) string {
+		if i%2 == 0 {
+			return "EVEN"
+		}
+		return "ODD"
+	}
+	numbers := []int{1, 2, 3, 4}
+	odsEvensMap := genfuncs.AssociateWith(numbers, oddEven)
+	fmt.Println(odsEvensMap[2]) // EVEN
+	fmt.Println(odsEvensMap[3]) // ODD
+}
+
+func ExampleDistinct() {
+	values := []int{1, 2, 2, 3, 1, 3}
+	fmt.Println(genfuncs.Distinct(values)) // [1 2 3]
+}
+
+func ExampleFlatMap() {
+	slicer := func(s string) []string { return strings.Split(s, "") }
+	fmt.Println(genfuncs.FlatMap(words.SortBy(lexicalOrder), slicer)) // [h e l l o w o r l d]
+}
+
+func ExampleFold() {
+	numbers := []int{1, 2, 3, 4, 5}
+	sum := func(a int, b int) int { return a + b }
+	fmt.Println(genfuncs.Fold(numbers, 0, sum)) // 15
+}
+
+func ExampleGroupBy() {
+	oddEven := func(i int) string {
+		if i%2 == 0 {
+			return "EVEN"
+		}
+		return "ODD"
+	}
+	numbers := []int{1, 2, 3, 4}
+	grouped := genfuncs.GroupBy(numbers, oddEven)
+	fmt.Println(grouped["ODD"]) // [1 3]
+}
+
+func ExampleMap() {
+	numbers := []int{69, 88, 65, 77, 80, 76, 69}
+	toString := func(i int) string { return string(rune(i)) }
+	fmt.Println(genfuncs.Map(numbers, toString)) // [E X A M P L E]
+}
+
+func ExampleSort() {
+	var letters genfuncs.Slice[string] = strings.Split("example", "")
+
+	letters.Sort(lexicalOrder)
+	fmt.Println(letters) // [a e e l m p x]
+	letters.Sort(reverseLexical)
+	fmt.Println(letters) // [x p m l e e a]
+}
---
+++ slice_functions_test.go
@@ -0,0 +1,302 @@
+/*
+ *  Copyright (c) 2021,  nwillc@gmail.com
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package genfuncs_test
+
+import (
+	"github.com/nwillc/genfuncs"
+	"github.com/stretchr/testify/assert"
+	"strconv"
+	"testing"
+)
+
+func TestAssociate(t *testing.T) {
+	var firstLast genfuncs.KeyValueFor[PersonName, string, string] = func(p PersonName) (string, string) { return p.First, p.Last }
+	type args struct {
+		slice     []PersonName
+		transform genfuncs.KeyValueFor[PersonName, string, string]
+	}
+	tests := []struct {
+		name     string
+		args     args
+		wantSize int
+		contains []string
+	}{
+		{
+			name: "Empty",
+			args: args{
+				slice:     []PersonName{},
+				transform: firstLast,
+			},
+			wantSize: 0,
+		},
+		{
+			name: "Two Unique",
+			args: args{
+				slice: []PersonName{
+					{
+						First: "fred",
+						Last:  "flintstone",
+					},
+					{
+						First: "barney",
+						Last:  "rubble",
+					},
+				},
+				transform: firstLast,
+			},
+			wantSize: 2,
+			contains: []string{"fred", "baarney"},
+		},
+		{
+			name: "Duplicate",
+			args: args{
+				slice: []PersonName{
+					{
+						First: "fred",
+						Last:  "flintstone",
+					},
+					{
+						First: "fred",
+						Last:  "astaire",
+					},
+				},
+				transform: firstLast,
+			},
+			wantSize: 1,
+			contains: []string{"fred"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fNameMap := genfuncs.Associate(tt.args.slice, tt.args.transform)
+			assert.Equal(t, tt.wantSize, len(fNameMap))
+			for k, _ := range fNameMap {
+				_, ok := fNameMap[k]
+				assert.True(t, ok)
+			}
+		})
+	}
+}
+
+func TestAssociateWith(t *testing.T) {
+	var valueSelector genfuncs.ValueFor[int, int] = func(i int) int { return i * 2 }
+	type args struct {
+		slice     []int
+		transform genfuncs.ValueFor[int, int]
+	}
+	tests := []struct {
+		name     string
+		args     args
+		wantSize int
+	}{
+		{
+			name: "Empty",
+			args: args{
+				slice:     []int{},
+				transform: valueSelector,
+			},
+			wantSize: 0,
+		},
+		{
+			name: "Three Unique",
+			args: args{
+				slice:     []int{1, 2, 3},
+				transform: valueSelector,
+			},
+			wantSize: 3,
+		},
+		{
+			name: "Duplicate",
+			args: args{
+				slice:     []int{1, 2, 2},
+				transform: valueSelector,
+			},
+			wantSize: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resultMap := genfuncs.AssociateWith(tt.args.slice, tt.args.transform)
+			assert.Equal(t, tt.wantSize, len(resultMap))
+			for _, k := range tt.args.slice {
+				v, ok := resultMap[k]
+				assert.True(t, ok, "did not find key:", k)
+				assert.Equal(t, k*2, v)
+			}
+		})
+	}
+}
+
+func TestDistinct(t *testing.T) {
+	type args struct {
+		slice []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "Empty",
+			args: args{
+				slice: []int{},
+			},
+			want: []int{},
+		},
+		{
+			name: "No Duplicates",
+			args: args{
+				slice: []int{1, 2, 3},
+			},
+			want: []int{1, 2, 3},
+		},
+		{
+			name: "Duplicates",
+			args: args{
+				slice: []int{1, 2, 3, 1, 1, 2, 3, 3, 3},
+			},
+			want: []int{1, 2, 3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			distinct := genfuncs.Distinct(tt.args.slice)
+			assert.Equal(t, len(tt.want), len(distinct))
+		})
+	}
+}
+
+func TestFlatMap(t *testing.T) {
+	var trans genfuncs.Function[int, []string] = func(i int) []string { return []string{"#", strconv.Itoa(i)} }
+	type args struct {
+		slice     genfuncs.Slice[int]
+		transform genfuncs.Function[int, []string]
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "Empty",
+			args: args{
+				slice:     []int{},
+				transform: trans,
+			},
+			want: []string{},
+		},
+		{
+			name: "List",
+			args: args{
+				slice:     []int{1, 2, 3},
+				transform: trans,
+			},
+			want: []string{"#", "1", "#", "2", "#", "3"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := genfuncs.FlatMap(tt.args.slice, tt.args.transform)
+			assert.Equal(t, len(tt.want), len(got))
+			for _, s := range tt.want {
+				assert.True(t, genfuncs.Slice[string](got).Contains(s, genfuncs.OrderedComparator[string]()))
+			}
+		})
+	}
+}
+
+func TestFold(t *testing.T) {
+	si := []int{1, 2, 3}
+	sum := genfuncs.Fold(si, 10, func(r int, i int) int { return r + i })
+	assert.Equal(t, 16, sum)
+}
+
+func TestGroupBy(t *testing.T) {
+	type args struct {
+		slice       genfuncs.Slice[int]
+		keySelector genfuncs.KeyFor[int, string]
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string]genfuncs.Slice[int]
+	}{
+		{
+			name: "Odds Evens",
+			args: args{
+				slice: []int{1, 2, 3, 4},
+				keySelector: func(i int) string {
+					if i%2 == 0 {
+						return "even"
+					}
+					return "odd"
+				},
+			},
+			want: map[string]genfuncs.Slice[int]{"odd": {1, 3}, "even": {2, 4}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resultsMap := genfuncs.GroupBy(tt.args.slice, tt.args.keySelector)
+			assert.Equal(t, len(tt.want), len(resultsMap))
+			for k, v := range tt.want {
+				assert.True(t, v.All(func(i int) bool {
+					return genfuncs.Slice[int](resultsMap[k]).Contains(i, genfuncs.OrderedComparator[int]())
+				}))
+			}
+		})
+	}
+}
+
+func TestMap(t *testing.T) {
+	var trans genfuncs.Function[int, string] = strconv.Itoa
+	type args struct {
+		slice     genfuncs.Slice[int]
+		transform genfuncs.Function[int, string]
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "Empty",
+			args: args{
+				slice:     []int{},
+				transform: trans,
+			},
+			want: []string{},
+		},
+		{
+			name: "List",
+			args: args{
+				slice:     []int{1, 2, 3},
+				transform: trans,
+			},
+			want: []string{"1", "2", "3"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := genfuncs.Map(tt.args.slice, tt.args.transform)
+			assert.Equal(t, len(tt.want), len(got))
+			for _, s := range tt.want {
+				assert.True(t, genfuncs.Slice[string](got).Contains(s, genfuncs.OrderedComparator[string]()))
+			}
+		})
+	}
+}
