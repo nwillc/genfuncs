8a260825c730e53bbbd41611520c9f07bd594f7f
nwillc <nwillc@gmail.com>
2022-01-17 20:15:38 -0500

    test cooverage

--- dry_test.go
+++ dry_test.go
@@ -91,3 +91,175 @@ func TestIsEqualComparable(t *testing.T) {
 		})
 	}
 }
+
+func TestGreaterThanOrdered(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Greater",
+			args: args{
+				a: 2,
+				b: 1,
+			},
+			want: true,
+		},
+		{
+			name: "Equal",
+			args: args{
+				a: 1,
+				b: 1,
+			},
+			want: false,
+		},
+		{
+			name: "Less",
+			args: args{
+				a: 0,
+				b: 1,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := genfuncs.GreaterThanOrdered(tt.args.a, tt.args.b)
+			assert.Equal(t, v, tt.want, v)
+		})
+	}
+}
+
+func TestIsGreaterThanOrdered(t *testing.T) {
+	type args struct {
+		a genfuncs.Function[int, bool]
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Greater",
+			args: args{
+				a: genfuncs.IsGreaterThanOrdered(2),
+				b: 1,
+			},
+			want: true,
+		},
+		{
+			name: "Equal",
+			args: args{
+				a: genfuncs.IsGreaterThanOrdered(1),
+				b: 1,
+			},
+			want: false,
+		},
+		{
+			name: "Less",
+			args: args{
+				a: genfuncs.IsGreaterThanOrdered(0),
+				b: 1,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := tt.args.a(tt.args.b)
+			assert.Equal(t, v, tt.want, v)
+		})
+	}
+}
+
+func TestLessThanOrdered(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Greater",
+			args: args{
+				a: 2,
+				b: 1,
+			},
+			want: false,
+		},
+		{
+			name: "Equal",
+			args: args{
+				a: 1,
+				b: 1,
+			},
+			want: false,
+		},
+		{
+			name: "Less",
+			args: args{
+				a: 0,
+				b: 1,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := genfuncs.LessThanOrdered(tt.args.a, tt.args.b)
+			assert.Equal(t, v, tt.want, v)
+		})
+	}
+}
+
+func TestIsLessThanOrdered(t *testing.T) {
+	type args struct {
+		a genfuncs.Function[int, bool]
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Greater",
+			args: args{
+				a: genfuncs.IsLessThanOrdered(2),
+				b: 1,
+			},
+			want: false,
+		},
+		{
+			name: "Equal",
+			args: args{
+				a: genfuncs.IsLessThanOrdered(1),
+				b: 1,
+			},
+			want: false,
+		},
+		{
+			name: "Less",
+			args: args{
+				a: genfuncs.IsLessThanOrdered(0),
+				b: 1,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := tt.args.a(tt.args.b)
+			assert.Equal(t, v, tt.want, v)
+		})
+	}
+}
