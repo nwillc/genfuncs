24fbfa4d6378f3bf90f9f9f0969a36f75a0ac879
nwillc <nwillc@gmail.com>
2022-06-26 12:40:26 -0400

    test coverage

---
+++ container/iterator_test.go
@@ -0,0 +1,31 @@
+/*
+ *  Copyright (c) 2022,  nwillc@gmail.com
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package container_test
+
+import (
+	"github.com/nwillc/genfuncs"
+	"github.com/nwillc/genfuncs/container"
+	"github.com/nwillc/genfuncs/container/sequences"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewIteratorSequence(t *testing.T) {
+	seq1 := sequences.SequenceOf(1, 2, 3)
+	seq2 := container.NewIteratorSequence(seq1.Iterator())
+	assert.Equal(t, genfuncs.EqualTo, sequences.Compare[int](seq1, seq2, genfuncs.Ordered[int]))
+}
